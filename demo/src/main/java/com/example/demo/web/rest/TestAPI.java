e7b265fa806fbb31bb31d5c316ff3eec03f39dd3
Minh Tri <daominhtri1000@gmail.com>
2023-02-26 21:23:38 +0700

    add demo spring boot project

---
+++ demo/src/main/java/com/example/demo/web/rest/TestAPI.java
@@ -0,0 +1,16 @@
+package com.example.demo.web.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestAPI {
+
+    @GetMapping("/")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Testing here.");
+    }
+}
